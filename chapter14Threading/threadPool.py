84ba846f4a1262753e23d330364879343632439e
xueshan007050 <45525769+xueshan007050@users.noreply.github.com>
2019-11-19 16:59:47 +0800

    修改threadPool程序

--- chapter14Threading/threadPool.py
+++ chapter14Threading/threadPool.py
@@ -13,6 +13,14 @@ Executor提供常用方法：
 3、shutdown(wait=True):关闭线程池
 -----------------------
 提交submit给线程池后，submit方法会返回一个Future对象，Future类主要用于获取线程任务函数的返回值。
+Future提供了如下方法：
+cancel():取消该Future代表的线程任务。如果该任务正在执行，不可取消，则返回False；否则会取消该任务，返回True。
+cancelled():返回Future线程是否被取消
+running():如果该Future代表的线程任务正在执行、不可被取消，该方法返回True
+done():如果该Future代表的线程任务被成功取消或执行完成，则该方法返回True
+result(timeout=None):获取该Future代表的线程任务最后返回的结果。
+exception(timeout=None):
+add_done_callback(fn):
 
 '''
 from concurrent.futures import ThreadPoolExecutor
@@ -24,7 +32,7 @@ import time
 '''
 def action(max):
 	my_sum = 0
-	for i in range(max)：
+	for i in range(max):
 		print(threading.current_thread().name + " " +str(i))
 		my_sum +=i
 	return my_sum
@@ -32,7 +40,7 @@ def action(max):
 '''
 创建一个包含两个线程的线程池
 '''
-while ThreadPoolExecutor(max_workers=2) as pool:
+with ThreadPoolExecutor(max_workers=2) as pool:
 	#向线程池中提交一个任务，50会作为action()函数的参数
 	future1 = pool.submit(action,50)
 	#向线程池中再提交一个任务，100会作为action()函数的参数
